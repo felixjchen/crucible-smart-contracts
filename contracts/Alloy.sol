57573a64f330460cabb082ae067100e46d68c0f6
felix <31393977+felixjchen@users.noreply.github.com>
2025-04-27 19:43:27 -0400

    test

--- contracts/Alloy.sol
+++ contracts/Alloy.sol
@@ -4,7 +4,18 @@ pragma solidity ^0.8.22;
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 import { OFT } from "@layerzerolabs/oft-evm/contracts/OFT.sol";
 
-contract MyOFT is OFT {
+enum CollectionType {
+    ERC1155,
+    ERC721,
+    ERC20
+}
+
+contract Alloy is OFT {
+    address collection;
+    CollectionType collectionType;
+    uint256[] ids; // Used in ERC1155, ERC721
+    uint256[] amounts; // Used in ERC1155, ERC20
+
     constructor(
         string memory _name,
         string memory _symbol,
